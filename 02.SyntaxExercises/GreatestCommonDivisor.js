cc2c5ff693ba4055650cb0b0913a6dd6ac1ec4bd
GeorgiStoychev <61363718+Stoychev13@users.noreply.github.com>
2022-09-11 21:36:33 +0300

    Divisor

---
+++ 02.SyntaxExercises/GreatestCommonDivisor.js
@@ -0,0 +1,28 @@
+function GCD(firstNum, secondNum){
+    let divisor;
+    let duvNumber;
+
+    if(firstNum > secondNum){
+        divisor = secondNum;
+        duvNumber = firstNum;
+    }else{
+        divisor = firstNum;
+        duvNumber = secondNum;
+    }
+    let finalResult;
+
+    while(true){
+        finalResult = duvNumber % divisor;
+        if(finalResult != 0){
+            duvNumber = divisor;
+            divisor = finalResult;
+        }else{
+            break;
+        }
+    }
+
+    console.log(divisor);
+}
+
+GCD(15,5);
+GCD(2154, 458);
