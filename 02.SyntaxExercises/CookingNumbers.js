6a2aa5cdb6001bf0616447fb7d220d717cc3d6a1
GeorgiStoychev <61363718+Stoychev13@users.noreply.github.com>
2022-09-11 22:09:11 +0300

    Cooking By Numbers

---
+++ 02.SyntaxExercises/CookingNumbers.js
@@ -0,0 +1,17 @@
+function cookingByNumbers(number, firstOperator, secondOperator, thirdOperator, forthOperaor, fiftOperaor){
+    let operators = [firstOperator, secondOperator, thirdOperator, forthOperaor, fiftOperaor];
+
+    for(let i = 0; i <= operators.length -1; i++){
+        switch(operators[i]){
+            case 'chop' : number /= 2; break;
+            case 'dice' : number=Math.sqrt(number); break;
+            case 'spice': number += 1; break;
+            case 'bake' : number *= 3; break;
+            case 'fillet': number -= number *= 0.2; break;
+        }
+        console.log(number);
+    }
+}
+
+cookingByNumbers('32', 'chop', 'chop', 'chop', 'chop', 'chop');
+cookingByNumbers('9', 'dice', 'spice', 'chop', 'bake', 'fillet')
