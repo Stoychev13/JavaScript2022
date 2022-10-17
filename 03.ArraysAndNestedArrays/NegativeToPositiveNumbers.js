c05d906bcdd96c95501cc9e8a47594a4fdfaad64
GeorgiStoychev <61363718+Stoychev13@users.noreply.github.com>
2022-10-17 14:24:44 +0300

    Bigger half

---
+++ 03.ArraysAndNestedArrays/NegativeToPositiveNumbers.js
@@ -0,0 +1,14 @@
+function negativeToPositiveNumbers(input){
+    let result = [];
+
+    for(let number of input){
+        if(number < 0){
+            result.unshift(number);
+        }else {
+            result.push(number);
+        }
+    }
+    for(let number of result){
+        console.log(number)
+    }
+}
