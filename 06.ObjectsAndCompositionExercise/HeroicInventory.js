44442906bc4c31db867d1c2c73c3ccdecb0c670d
GeorgiStoychev <61363718+Stoychev13@users.noreply.github.com>
2022-10-18 07:50:22 +0300

    Heroic Inventory

---
+++ 06.ObjectsAndCompositionExercise/HeroicInventory.js
@@ -0,0 +1,19 @@
+function solve(array) {
+
+    const heroInventary = [];
+
+    for (let index = 0; index < array.length; index++) {
+        let splitHero = array[index].split(' / ');
+        let name = splitHero[0];
+        let level = Number(splitHero[1]);
+        let items = splitHero.length > 2 ? splitHero[2].split(', ') : [];
+
+
+        heroInventary[index] = {
+            name, level, items
+        }
+    }
+
+
+    return JSON.stringify(heroInventary);
+}
