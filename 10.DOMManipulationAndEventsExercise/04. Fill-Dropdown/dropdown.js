33162612d2bf493d1077ec07f55438a67f3830c9
GeorgiStoychev <61363718+Stoychev13@users.noreply.github.com>
2022-10-20 22:26:54 +0300

    Fill Dropdown

--- 10.DOMManipulationAndEventsExercise/04. Fill-Dropdown/dropdown.js
+++ 10.DOMManipulationAndEventsExercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    const menu = document.querySelector('#menu');
+
+
+    let newItemText = document.querySelector('#newItemText').value;
+    let newItemValue = document.querySelector('#newItemValue').value;
+
+    const option = document.createElement('option')
+    option.textContent = newItemText;
+    option.value = newItemValue;
+    menu.add(option);
+
+    document.querySelector('#newItemText').value = '';
+    document.querySelector('#newItemValue').value = '';
 }
