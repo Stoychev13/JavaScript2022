6e195aeb371e2a43927f20f2dc94e3188f3c3561
GeorgiStoychev <61363718+Stoychev13@users.noreply.github.com>
2022-10-20 21:57:09 +0300

    Table – Search Engine

--- 08.DOMExercises/05. Table - Search Engine/solution.js
+++ 08.DOMExercises/05. Table - Search Engine/solution.js
@@ -2,7 +2,16 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let searchingFor = document.getElementById('searchField').value;
+
+      const array = document.querySelectorAll('tbody tr');
+
+      for(let index=0; index<array.length; index++){
+         let row = array[index];
+         if(row.textContent.includes(searchingFor)){
+            row.className = 'select';
+         }
+      }
 
    }
 }
